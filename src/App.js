29f5c5f990eaef6f875a620446857a3252f9c565
mert <uzeyirmertciflikli@gmail.com>
2020-12-04 11:40:46 +0100

    copy the state array instead of the reference

--- src/App.js
+++ src/App.js
@@ -17,13 +17,13 @@ class App extends Component {
   }
   
   handleAllChecked = (event) => {
-    let fruites = this.state.fruites
+    let fruites = [...this.state.fruites]
     fruites.forEach(fruite => fruite.isChecked = event.target.checked) 
     this.setState({fruites: fruites})
   }
 
   handleCheckChieldElement = (event) => {
-    let fruites = this.state.fruites
+    let fruites = [...this.state.fruites]
     fruites.forEach(fruite => {
        if (fruite.value === event.target.value)
           fruite.isChecked =  event.target.checked
